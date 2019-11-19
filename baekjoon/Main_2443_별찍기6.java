d70af5e104067663cd5cb891890c762485870e95
Hdong27 <yhd943@naver.com>
2019-11-19 17:45:24 +0900

    update

---
+++ baekjoon/Main_2443_별찍기6.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2443_º°Âï±â6 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j = 0; j < 2 * (N - i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
